0f996eea1c180397ff4e1fce087423819c33b0ca
Cayden Townsend <cayde@Mac.lan>
2026-03-25 20:05:22 -0600

    dude

--- database.py
+++ database.py
@@ -14,10 +14,19 @@ def get_student_flights(user_id):
     return supabase.table("flights").select("*").eq("student_id", user_id).execute()
 
 def get_flight_rate(flight_id):
-    flight = supabase.table("flights").select("*").eq("id", flight_id).single().execute()
-    if flight.data:
-        rate_id = flight.data["rate_id"]
-        return supabase.table("rates").select("*").eq("id", rate_id).single().execute().data
+    try:
+        flight = supabase.table("flights").select("rate_id").eq("id", flight_id).single().execute()
+    except APIError:
+        return None
+
+            rate_id = flight.data.get("rate_id")
+        if not rate_id:
+            return None
+
+        try:
+            return supabase.table("rates").select("*").eq("id", rate_id).single().execute().data
+        except APIError:
+            return None
     return None
 
 def get_student_achievements(student_id):
